3f881c9a9614408aa1e1553a6a0f429992fd4775
Ry4c <131599009+Ry4c@users.noreply.github.com>
2023-05-10 21:50:31 +0700

    demo dien tich

---
+++ jsdemo/JS-demo3.js
@@ -0,0 +1,8 @@
+let width;
+let height;
+width = prompt('nhap chieu dai');
+height = prompt('nhap chieu rong');
+let w = parseInt(width);
+let h = parseInt(height);
+let s = w * h;
+document.write(s);
