9be3e65d696cbe29fd1f14a7abea60921252f7c1
Ry4c <131599009+Ry4c@users.noreply.github.com>
2023-05-11 15:22:05 +0700

    up den bai login

---
+++ jsdemo/js-doitien.js
@@ -0,0 +1,12 @@
+
+function doi() {
+    let a = document.getElementById('nhap').value;
+    let b = document.getElementById('fcurrency').value;
+    let c = document.getElementById('tcurrency').value;
+    let kq = document.getElementById('kq');
+    let n = parseFloat(a);
+    let m = parseFloat(b);
+    let p = parseFloat(c);
+    let q = n / p * m;
+    kq.innerText=q;
+}
